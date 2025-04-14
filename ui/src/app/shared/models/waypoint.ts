e608b1cb3ad2c4e180c865d415a3283ace10e5c5
esan0n <sanonerwin@gmail.com>
2025-04-14 17:31:34 +0200

    Internationalefahrt ins richtige Verzeichniss verschoben

--- ui/src/app/shared/models/waypoint.ts
+++ ui/src/app/shared/models/waypoint.ts
@@ -1,9 +1,9 @@
-import {InternationaleFahrt} from "./internationalefahrt";
+import {InternationaleFahrt} from "./internationalefahrt"
 export interface Waypoint {
     latitude: number;
     longitude: number;
     timestamp: number;
-    internationaleFahrten: InternationaleFahrt[];
+    internationaleFahrt: InternationaleFahrt[];
 
   }
   
